066cb20bfba5343ea592088db0cbac324ee09117
George Peristerakis <peristerakis@gmail.com>
2018-02-19 00:04:55 -0500

    Hard code the admin url

--- config/settings/production.py
+++ config/settings/production.py
@@ -214,7 +214,7 @@ RAVEN_CONFIG = {
 }
 
 # Custom Admin URL, use {% url 'admin:index' %}
-ADMIN_URL = env('DJANGO_ADMIN_URL')
+ADMIN_URL = r'^admin/'
 
 # Your production stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
