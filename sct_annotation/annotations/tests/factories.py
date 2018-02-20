0f9e973a7029b10f51cd58990afd11cae56ec73c
George Peristerakis <peristerakis@gmail.com>
2018-02-20 09:08:31 -0500

    Added a skeleton test folder

--- sct_annotation/annotations/tests/factories.py
+++ sct_annotation/annotations/tests/factories.py
@@ -2,24 +2,25 @@ import datetime
 import factory
 import factory.fuzzy as fuzzy
 
+from ..models import Acquisition, Demographic, Image
+
 
 class AcquisitionFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Acquisition
+
     center = factory.Sequence(lambda n: 'center-{}'.format(n))
     study = factory.Sequence(lambda n: 'study-{}'.format(n))
     subject = factory.Sequence(lambda n: 'subject-{}'.format(n))
 
-    class Meta:
-        model = 'annotations.Acquisition'
-
 
 class DemographicFactory(factory.django.DjangoModelFactory):
-    subject = factory.SubFactory(AcquisitionFactory)
-    nifti_path = fuzzy.FuzzyText()
-    created_date = fuzzy.FuzzyDateTime(datetime.datetime(2008, 1, 1, tzinfo=datetime.timezone.utc))
-
     class Meta:
-        model = 'annotations.Demographic'
+        model = Demographic
 
 
 class ImageFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Image
+
     contrast = fuzzy.FuzzyChoice(['t1', 't2', 't2s'])
--- sct_annotation/annotations/tests/test_models.py
+++ sct_annotation/annotations/tests/test_models.py
@@ -11,11 +11,12 @@ def dummy(*args):
 
 class TestSubject(TestCase):
     def setUp(self):
-        factories.AcquisitionFactory()
+        acq = factories.AcquisitionFactory()
+        demo = factories.DemographicFactory(acquisition=acq)
 
     def test_default_dict(self):
         subject = Acquisition.objects.all()[0]
         assert str(subject) == 'center-0_study-0_subject-0'
-        # assert subject.to_dict()['center']
+        sub_dict = subject.to_dict()
         # self.assertTrue(subject.to_dict()['study'])
         # self.assertTrue(subject.to_dict()['subject'])
